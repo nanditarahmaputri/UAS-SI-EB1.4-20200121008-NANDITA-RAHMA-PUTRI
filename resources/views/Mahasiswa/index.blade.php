7947712f60c9ee962eb188973d56c31dd1d86852
nanditarahmaputri <nanditananditarahma285@gmail.com>
2022-08-05 17:06:53 +0700

    commit2

--- resources/views/Mahasiswa/index.blade.php
+++ resources/views/Mahasiswa/index.blade.php
@@ -1,4 +1,25 @@
 @extends('../template/main')
 
 @section('content')
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nama</th>
+                <th scope="col">Alamat</th>
+                <th scope="col">Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($mahasiswa as $data_mahasiswa)
+                <tr>
+                    <th>1</th>
+                    <td>{{ $data_mahasiswa->nama_mahasiswa }}</td>
+                    <td>{{ $data_mahasiswa->nama_mahasiswa }}</td>
+                    <td>{{ $data_mahasiswa->nama_mahasiswa }}</td>
+
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
